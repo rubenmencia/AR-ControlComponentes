36b9fe6b5fca3d9e1e2c240cfd400ad1e5605c70
Grey Sailor <39842274+cyklophe@users.noreply.github.com>
2020-05-09 14:09:34 +0200

    Corregir pin de control

--- AR00004_001.ino
+++ AR00004_001.ino
@@ -7,7 +7,7 @@ int pos = 0;    // variable to store the servo position
 
 void setup() {
   Serial.begin(9600);
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(11);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
